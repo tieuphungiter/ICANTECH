e83b2dc88899f7a52ea329f742e66653076cfb00
tieuphungiter <tieuphungiter@gmail.com>
2024-08-05 14:25:44 +0700

    Aug 5th update

--- Pro_Python_05/Unit_01/unit_01.py
+++ Pro_Python_05/Unit_01/unit_01.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv(r"Unit_01\unit_01_weather_data.csv")
+data = pd.read_csv(r"unit_01_weather_data.csv")
 
 # Kiểm tra số dòng hiển thị tối đa của hệ thống
 print(pd.options.display.max_rows)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ICANTECH
+
+This is a repository of projects in ICANTECH courses including Python and Web courses
